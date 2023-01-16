f561a5298462328bed9ece8481888dd265f54cae
Nikos Triantafyllou <triantafyllou.ni@gmail.com>
2023-01-16 12:03:02 +0200

    updated path rules resoultion

--- src/main/java/gr/aegean/palaemon/rulesengine/utils/PathSorter.java
+++ src/main/java/gr/aegean/palaemon/rulesengine/utils/PathSorter.java
@@ -7,6 +7,6 @@ import java.util.Comparator;
 public class PathSorter implements Comparator<Path> {
     @Override
     public int compare(Path set1, Path set2) {
-        return set1.getGeofences().size() - set2.getGeofences().size();
+        return set2.getGeofences().size() - set1.getGeofences().size();
     }
 }
